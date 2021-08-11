a66fd3fb7b0d5e8b2447810156f6d5b87f833d75
sanamaruno <xwkpm269@gmail.com>
2021-08-11 09:52:55 +0000

    [Add]バリデーション,フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,48 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    @book = Book.new(book_params)
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+def update
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+  else
+    render :show
+  end
+end
 
-  private
+def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to books_path, notice: 'Book was successfully destroyed.'
+end
+
+
+private
+
   def book_params
-    params.permit(:title, :body)
-
+    params.require(:book).permit(:title, :body)
   end
 
 end
